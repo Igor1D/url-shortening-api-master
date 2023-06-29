641d652800f44c12ddc77646e6956873e62a3243
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-06-29 21:46:08 +0200

    dynamic url, js div

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 
 async function shortenUrl() {
-    let results = await fetch('https://api.shrtco.de/v2/shorten?url=example.org/very/long/link.html');
+    let results = await fetch(`https://api.shrtco.de/v2/shorten?url=${input.value}`);
     let data = await results.json();
     let shortenLink = data.result.full_short_link3;
+    let urlDiv = document.createElement('div');
+    urlDiv.classList.add('urlDiv');
+    urlDiv.innerHTML = `<p class="longUrl">${input.value}</p> <p class="shortUrl">${shortenLink}</p> <button class="copyBtn">Copy</button>`;
+    document.body.appendChild(urlDiv);
     console.log(shortenLink);
 }
+
+
 
 
 
@@ -15,11 +21,8 @@ async function shortenUrl() {
 form.addEventListener('submit', (event) => {
     event.preventDefault()
 
-    
-
-
     shortenUrl();
-    // console.log(input.value);
+    console.log(input.value);
 
 });
 
