a7d62c385b97a7a79a39aa9eab33db7ffcd4a60f
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-07-05 22:08:50 +0200

    html and css - completed the shorten link div and started with the mobile version

--- index.js
+++ index.js
@@ -6,10 +6,33 @@ async function shortenUrl() {
     let data = await results.json();
     let shortenLink = data.result.full_short_link3;
     let urlDiv = document.createElement('div');
+    let shortenLinks = document.getElementById('shortenLinks');
+    
     urlDiv.classList.add('urlDiv');
-    urlDiv.innerHTML = `<p class="longUrl">${input.value}</p> <p class="shortUrl">${shortenLink}</p> <button class="copyBtn">Copy</button>`;
-    document.body.appendChild(urlDiv);
-    console.log(shortenLink);
+    urlDiv.innerHTML = `<p class="longUrl">${input.value}</p> <div class="shortUrlBtn"><p id="shortUrl">${shortenLink}</p> <button id="copyBtn" class="mainBtn" onclick="copyFunction()">Copy</button></div>`;
+    shortenLinks.appendChild(urlDiv);
+    let copyBtn = document.getElementById('copyBtn');
+    
+//     function copyFunction() {
+//         let shortUrl = document.getElementById('shortUrl');
+//         // Create a fake `textarea` and set the contents to the text
+//   //    you want to copy
+//         let storage = document.createElement('textarea');
+//         storage.value = shortUrl.innerHTML;
+//         shortUrl.appendChild(storage);
+//         // Copy the text in the fake `textarea` and remove the `textarea`
+//         storage.select();
+//         storage.setSelectionRange(0, 99999);
+//         document.execCommand('copy');
+//         element.removeChild(storage);
+
+
+
+
+//     }
+    
+    console.log(copyBtn)
+
 }
 
 
@@ -18,7 +41,6 @@ form.addEventListener('submit', (event) => {
     event.preventDefault()
 
     shortenUrl();
-    console.log(input.value);
-
+    
 });
 
