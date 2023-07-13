7d40b7a1f65ace31b5f15a57f416e1613168ee33
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-07-13 21:50:32 +0200

    copy button plus mobiles css fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-// let mobileMenuBtn = document.getElementById('menu-toggle');
-// let nav = document.getElementById('nav');
+let mobileMenuBtn = document.getElementById('menu-icon');
+let nav = document.getElementById('nav');
 
-// mobileMenuBtn.addEventListener('click', ()=>{
-//     nav.style.display = 'none';
+mobileMenuBtn.addEventListener('click', ()=>{
+    nav.classList.toggle('navShown');
     
-// })
+});
 
 
 async function shortenUrl() {
@@ -15,39 +15,23 @@ async function shortenUrl() {
     let shortenLink = data.result.full_short_link3;
     let urlDiv = document.createElement('div');
     let shortenLinks = document.getElementById('shortenLinks');
-    let copyBtn = document.getElementById('copyBtn');
+    
     let shortUrl = document.getElementById('shortUrl');
     
     urlDiv.classList.add('urlDiv');
-    urlDiv.innerHTML = `<p class="longUrl">${input.value}</p> <div class="shortUrlBtn"><p id="shortUrl">${shortenLink}</p> <button id="copyBtn" class="mainBtn" onclick="copyFunction()">Copy</button></div>`;
+    urlDiv.innerHTML = `<p class="longUrl">${input.value}</p> <div class="shortUrlBtn"><p id="shortUrl">${shortenLink}</p> <button id="copyBtn" class="mainBtn">Copy</button></div>`;
     shortenLinks.appendChild(urlDiv);
+    input.value = '';
+    let copyBtn = document.getElementById('copyBtn');
+    
+    copyBtn.addEventListener('click', ()=> {
+        navigator.clipboard.writeText(shortenLink);
+        copyBtn.innerHTML = 'Copied!';
+        copyBtn.style.backgroundColor = 'hsl(255, 11%, 22%)';
+    })
+
     
 
-    // copyBtn.addEventListener('click', ()=> {
-    //     let storage = document.createElement('textarea');
-    //     storage.value = shortUrl.innerText;
-    //     shortUrl.appendChild(storage);
-    //     storage.select();
-    //     document.execCommand('copy');
-    //     document.body.removeChild(storage);
-    //     copyBtn.innerHTML = 'Copied!';
-    //     setTimeout(() => {
-    //         copyBtn.innerText = 'Copy';
-    //       }, 2000);
-    // })
-
-    // function copyFunction(){
-    //     let storage = document.createElement('textarea');
-    //     storage.value = shortUrl.innerText;
-    //     shortUrl.appendChild(storage);
-    //     storage.select();
-    //     document.execCommand('copy');
-    //     document.body.removeChild(storage);
-    //     copyBtn.innerHTML = 'Copied!';
-    //     setTimeout(() => {
-    //         copyBtn.innerText = 'Copy';
-    //       }, 2000);
-    // }
 
 }
 
@@ -61,11 +45,6 @@ form.addEventListener('submit', (event) => {
     
 });
 
-// copyBtn.addEventListener('click', ()=> {
-
-//     copyFunction();
-
-// })
 
 
 
